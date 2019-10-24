63347882344aed07c1cc73c1f43b764b4e555941
Geek Mode <jcromerohdz@gmail.com>
2019-10-24 09:29:36 -0700

    Rspec tho enumerable in my_each method

---
+++ spec/enumarable_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../scripts/enumerable.rb'
+
+RSpec.describe 'Enumerable' do
+  it 'goes through all elments in the list and add 1' do
+    a = [1,2,3]
+    r = []
+    a.my_each {|i| r << i + 1}
+    expect(r).to eq([2,3,4])
+
+  end
+
+  it 'goes through all elments in the list and times 2' do
+    a = [1,2,3]
+    r = []
+    a.my_each {|i| r << i * 2}
+    expect(r).to eq([2,4,6])
+
+  end
+
+  it "returns an enumerable if not passed a block" do
+    expect([1,2,3].my_each).to be_a(Enumerable)
+  end
+end
